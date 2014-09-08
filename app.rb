dea9c5b9b85f2d6abb777368749fbb53a63a5966
adesuwau <missadeugbogbo@gmail.com>
2014-09-08 19:53:27 -0400

    Adds Twitter API and first instances of Instagram API

--- app.rb
+++ app.rb
@@ -28,6 +28,7 @@ class App < Sinatra::Base
     $redis = Redis.new({:host => uri.host,
                         :port => uri.port,
                         :password => uri.password})
+    CALLBACK_URL = "http://127.0.0.1:9292/oauth_callback"
   @@profiles = []
   end
 
@@ -68,19 +69,39 @@ get("/oauth_callback") do
     session["access_token"] = response["access_token"]
     user_info_response = HTTParty.get("https://api.github.com/user?access_token=#{session['access_token']}", headers: { "User-Agent" => "Rat Store Example" })
     # binding.pry
-  end
+  response_two = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+  session[:access_token] = response_two.access_token
+ end
   redirect to("/profile/edit")
 end
 
+get "/oauth/connect" do
+  redirect Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+end
+
+
+
 get("/feeds")do
+#################
+# NYTimes Events
+#################
+
   @events = HTTParty.get("http://api.nytimes.com/svc/events/v2/listings.json?filters=borough:Manhattan&api-key=d580e2fba62b85adae01dbb42834ddab:6:69766004")
   @simplified_events = @events["results"]
+
+#####################
+# Weather Underground
+#####################
 
     @city = "new_york"
     @state = "NY"
       @hourly_temperature = HTTParty.get("http://api.wunderground.com/api/#{WEATHERUG_KEY}/hourly/q/#{@state}/#{@city}.json")
       first_time = @hourly_temperature["hourly_forecast"][0]["FCTTIME"]["civil"]
       first_temp = @hourly_temperature["hourly_forecast"][0]["temp"]["english"]
+
+###############
+# Yelp
+###############
 
       @client = Yelp::Client.new({ consumer_key: "Tk51e10C3NlC-bpMio_orA",
                             consumer_secret: "jR0kGr2xOX5GMuWZnYIlF_KGeOk",
@@ -91,9 +112,16 @@ get("/feeds")do
       @stringy_ny_yelp = @ny_yelp.to_json
       @parsed_ny_yelp = JSON.parse(@stringy_ny_yelp)
 
+#######################
+# NYTimes Movie Reviews
+#######################
+
 @reviews = HTTParty.get("http://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&api-key=1e0225a5429d17924a7526f4a9454f9c:10:69766004")
 @simplified_reviews = @reviews["results"]
 
+##############
+# Twitter
+##############
 
 @twitter_client              = Twitter::REST::Client.new do |config|
   config.consumer_key        = "VtC6Dir0O3m0tJudSs4gdlq12"
@@ -101,12 +129,18 @@ get("/feeds")do
   # config.access_token        = "172149629-E0uBw812dgzlkN8JT9NwKfCwnlbYg6YnJeuWlfdk"
   # config.access_token_secret = "4Cs54fbL6RRXv4Vv0E7I8tV6xYxc7tCpGO3v1gzPcb23w"
 end
-@tweets = @twitter_client.search
+@tweets = @twitter_client.search("nightlife, nyc", :result_type => "recent").take(5).collect do |tweet|
+      {content: "#{tweet.user.screen_name}: #{tweet.text}"}
+    end
+###############
+# Instagram
+###############
+    Instagram.configure do |config|
+      config.client_id = "2f02f71d330647768ec32f4da1ef1df6"
+      config.client_secret = "b7014ef40e3c424e94e659632b5d866c"
+    end
 
-# Instagram.configure do |config|
-#   config.client_id = "2f02f71d330647768ec32f4da1ef1df6"
-#   config.client_secret = "b7014ef40e3c424e94e659632b5d866c"
-# end
+@ig_flicks = HTTParty.get("https://api.instagram.com/v1/media/popular?access_token=#{session['access_token']}")
 
   render(:erb, :dashboard, :template =>:layout)
 end
