bf6ab208c6d5283cdb6b45c39b6a1d688fb16ed9
adesuwau <missadeugbogbo@gmail.com>
2014-09-09 13:49:34 -0400

    Adds first implementation of header and dashboard styling

--- app.rb
+++ app.rb
@@ -70,8 +70,8 @@ get("/oauth_callback") do
     session["access_token"] = response["access_token"]
     user_info_response = HTTParty.get("https://api.github.com/user?access_token=#{session['access_token']}", headers: { "User-Agent" => "Rat Store Example" })
     # binding.pry
-  response_two = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
-  session[:access_token] = response_two.access_token
+#     response_two = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+#     session[:access_token] = response_two.access_token
  end
   redirect to("/profile/edit")
 end
