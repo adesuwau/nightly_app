3b85456afc2ea571de903f5ebb4c2311db70c721
adesuwau <missadeugbogbo@gmail.com>
2014-09-10 11:23:23 -0400

    Changes some main page styling and tests

--- spec/nightly_app_spec.rb
+++ spec/nightly_app_spec.rb
@@ -8,27 +8,11 @@ describe("The Main Page")do
     expect(page).to have_content("night.ly")
   end
 
-  it("requests that the user register")do
-  visit("/")
-  expect(page).to have_content ("register")
+  it("requests that the user login with github")do
+    visit("/")
+    expect(page).to have_content ("Login With Github")
   end
 
-    it("requests that the user login with github")do
-  visit("/")
-  expect(page).to have_content ("Login With Github")
-  end
 end
 
-describe("Profile Edit")do
 
-  it("gives a total")do
-  visit("/")
-  click_on "Calculate Rats"
-  #when we click on links we can be redirected
-  #now we are on rat store
-  number_of_rats = (1..100).to_a.sample
-  fill_in "Quantity of Rats", with: number_of_rats
-  click_button "Calculate"
-  expect(page).to have_content "That's $#{number_of_rats * 10} worth of rats!"
-  end
-end
